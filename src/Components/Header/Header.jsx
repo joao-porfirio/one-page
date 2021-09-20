196a3e87ef8e0e20eda41ea35ffe64d6a1bb649a
Joao Porfirio <joaoprf54@gmail.com>
2021-09-20 19:12:37 -0300

    Ajustes de tags para semântica

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -5,22 +5,20 @@ import header_img from '../../assets/img/image.svg'
 
 const Header = () => {
   return (
-    // <div>
-      <header className="header">
-        <div className="header__column">
-          <article className="header__conteudo">
-            <h1 className="header__titulo">Lorem ipsum dolor sit amet</h1>
-            <p className="header__descricao">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas eu massa sit amet augue consectetur ullamcorper non ac massa.</p>
-            <button className="header__button">Botão</button>
-          </article>
-         
-
-        </div>
-        <div className="header__column">
-          <img className="header__imagem" src={header_img} alt="Banner" />
-        </div>
-      </header>
-    // </div>
+    <header className="header">
+      <div className="header__column">
+        <article className="header__conteudo">
+          <h1 className="header__titulo">Lorem ipsum dolor sit amet</h1>
+          <p className="header__descricao">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas eu massa sit amet augue consectetur ullamcorper non ac massa.</p>
+          <button className="header__button">Botão</button>
+        </article>
+      </div>
+      <div className="header__column">
+        <picture>
+          <img className="header__logo" src={header_img} alt="Banner" />
+        </picture>
+      </div>
+    </header>
   )
 }
 
