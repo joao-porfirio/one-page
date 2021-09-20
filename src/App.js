26b745e6f2217e9b76ad5796436a6e3d99016d8c
Joao Porfirio <joaoprf54@gmail.com>
2021-09-20 19:15:55 -0300

    Arquivo de renderização dos componentes do react

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import Footer from './Components/Footer/Footer';
 import Header from './Components/Header/Header'
+import Main from './Components/Main/Main'
+import './css/reset.css';
+import './css/style.css';
 function App() {
   return (
     <div className="App">
       <Header />
+      <Main />
+      <Footer />
     </div>
   );
 }
